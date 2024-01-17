50099e5f5aff0baa99e0febc93f09fb4ec58e908
caonguyen21 <106511965+caonguyen21@users.noreply.github.com>
2024-01-17 22:52:56 +0700

    done the payment upon receipt feature

--- controllers/ordersControllers.js
+++ controllers/ordersControllers.js
@@ -13,5 +13,45 @@ module.exports = {
         } catch (error) {
             res.status(500).json({ message: "Failed to get orders" })
         }
+    },
+    addUserOrders: async (req, res) => {
+        const { userId, productId, quantity, subtotal,  payment_status, total } = req.body;
+
+        try {
+            // Create a new order
+            const newOrder = new Orders({
+                userId,
+                productId,
+                quantity,
+                subtotal,
+                payment_status: 'unpaid',
+                total
+            });
+
+            // Save the new order to the database
+            const savedOrder = await newOrder.save();
+
+            res.status(201).json(savedOrder);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: "Failed to add order" });
+        }
+    },
+    deleteUserOrders: async (req, res) => {
+        const orderId = req.params.orderId; // Assuming orderId is part of the route parameters
+
+        try {
+            // Find the order by orderId and delete it
+            const deletedOrder = await Orders.findByIdAndDelete(orderId);
+
+            if (deletedOrder) {
+                res.status(200).json({ message: "Order deleted successfully" });
+            } else {
+                res.status(404).json({ message: "Order not found" });
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: "Failed to delete order" });
+        }
     }
 }
--- routes/orders.js
+++ routes/orders.js
@@ -3,4 +3,6 @@ const router = express.Router();
 const { verifyToken } = require('../middleware/verifyToken')
 const orderController = require('../controllers/ordersControllers');
 router.get('/', verifyToken, orderController.getUserOrders)
+router.post('/addOrder', verifyToken, orderController.addUserOrders)
+router.delete('/deleteOrder/:orderId', verifyToken, orderController.deleteUserOrders)
 module.exports = router;
